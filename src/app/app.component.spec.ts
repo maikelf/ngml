52a01e0600b5af725de0e8fe5558e3118f7c1d4b
Maikel Manuel Fernandez Fernandez <maikel@maikel.laptop>
2019-08-20 15:43:29 -0500

    fix text

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,13 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { Component } from '@angular/core';
 import { AppComponent } from './app.component';
+
+@Component({
+  selector: 'app-nav-bar',
+  template: ''
+})
+export class AppNavBarMockComponent {}
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -9,7 +16,8 @@ describe('AppComponent', () => {
         RouterTestingModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        AppNavBarMockComponent
       ],
     }).compileComponents();
   }));
@@ -18,18 +26,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'ngml'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ngml');
-  });
-
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ngml!');
   });
 });
